823e0986da116ea60c9d068cdcd0b12c201fdd62
Danylo Stolnikov <stolnikovdanil87@gmail.com>
2026-07-02 21:10:54 +0300

    refactor layout structure for improved responsiveness

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -19,9 +19,13 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-        <body className={`${inter.className} h-screen overflow-hidden`}>
+        <body className={`${inter.className} min-h-screen flex flex-col`}>
         <Header />
-        {children}
+
+        <main className="flex-1">
+            {children}
+        </main>
+
         <Footer />
         </body>
         </html>
