73060934116d1566ebfdf90372f2daa59a010bd7
Danylo Stolnikov <stolnikovdanil87@gmail.com>
2026-07-02 16:18:07 +0300

    fix: next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -9,6 +9,14 @@ const nextConfig: NextConfig = {
             },
         ],
     },
+    async rewrites() {
+        return [
+            {
+                source: "/api/:path*",
+                destination: `${process.env.BACKEND_URL}/:path*`,
+            },
+        ];
+    },
 };
 
 export default nextConfig;
